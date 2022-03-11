7ba3c385c993b90e442d35d67b6d16ade43635d5
HENQ42 <99844780+HENQ42@users.noreply.github.com>
2022-03-10 23:03:21 -0300

    master

--- bee1021.cpp
+++ bee1021.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-
- using namespace std;
+#include <cmath>
 
 int main(){
 
- double not100, not50, not20, not10, not5, not2, mod1, mod050, mod025, mod010, mod005, mod001, res1;
- int res;
+ int not100, not50, not20, not10, not5, not2, mod1, mod050, mod025, mod010, mod005, mod001, res;
+ double res1;
 
-cin >> res1;
+std::cin >> res1;
 
  res = res1;
  res1 = res1 - res;
@@ -25,19 +24,35 @@ cin >> res1;
     not2 = res / 2;
         res = res % 2;
 
-cout << "NOTAS:" << endl;
-cout << not100 << " nota(s) de R$ 100.00" << endl;
-cout << not50 << " nota(s) de R$ 50.00" << endl;
-cout << not20 << " nota(s) de R$20.00" << endl;
-cout << not10 << " nota(s) de R$10.00" << endl;
-cout << not5 << " nota(s) de R$5.00" << endl;
-cout << not2 << " nota(s) de R$2.00" << endl;
-cout << "MOEDAS:" << endl;
-cout << mod1 << " moeda(s) de R$ 1.00" << endl;
-cout << mod050 << " moeda(s) de R$ 0.50" << endl;
-cout << mod025 << " moeda(s) de R$ 0.25" << endl;
-cout << mod010 << " moeda(s) de R$ 0.10" << endl;
-cout << mod005 << " moeda(s) de R$ 0.05" << endl;
-cout << mod001 << " moeda(s) de R$ 0.01" << endl;
-                                
+res1 = res1 + res;
+
+res1 = res1 * 100;
+
+    mod1 = res1 / 100;
+     res1 = res1 - (mod1 * 100);
+    mod050 = res1 / 50;
+     res1 = res1 - (mod050 * 50);
+    mod025 = res1 / 25;
+     res1 = res1 - (mod025 * 25);
+    mod010 = res1 / 10;
+     res1 = res1 - (mod010 * 10);
+    mod005 = res1 / 5;
+     res1 = res1 - (mod005 * 5);
+
+    mod001 = round(res1);
+
+std::cout << "NOTAS:" << std::endl;
+std::cout << not100 << " nota(s) de R$ 100.00" << std::endl;
+std::cout << not50 << " nota(s) de R$ 50.00" << std::endl;
+std::cout << not20 << " nota(s) de R$ 20.00" << std::endl;
+std::cout << not10 << " nota(s) de R$ 10.00" << std::endl;
+std::cout << not5 << " nota(s) de R$ 5.00" << std::endl;
+std::cout << not2 << " nota(s) de R$ 2.00" << std::endl;
+std::cout << "MOEDAS:" << std::endl;
+std::cout << mod1 << " moeda(s) de R$ 1.00" << std::endl;
+std::cout << mod050 << " moeda(s) de R$ 0.50" << std::endl;
+std::cout << mod025 << " moeda(s) de R$ 0.25" << std::endl;
+std::cout << mod010 << " moeda(s) de R$ 0.10" << std::endl;
+std::cout << mod005 << " moeda(s) de R$ 0.05" << std::endl;
+std::cout << mod001 << " moeda(s) de R$ 0.01" << std::endl;                       
 }
