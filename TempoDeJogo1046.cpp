d276ca94e603c21b101e21288fd4bf28489d3467
HENQ42 <99844780+HENQ42@users.noreply.github.com>
2022-04-07 12:01:07 -0300

    master

---
+++ TempoDeJogo1046.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+ using namespace std;
+
+int main(){
+ int hi, hf, hr;
+cin >> hi >> hf;
+
+if(hi == hf){
+    hr = 24;
+}else{
+    if(hi > 12 && hf > 12){
+        hr = (hf + 12) - (hi - 12);
+    }else{
+        if(hi < 12 && hf > 12){
+            hr = hf - hi;
+        }else{
+            if(hi < 12 && hf < 12){
+                hr = hf - hi;
+            }else{
+                if(hi < 12 && hf < 12){
+                     hr = hi + hf;
+                }
+            }
+        }
+    }
+}
+cout << "O JOGO DUROU " << hr << " HORA(S)" << endl;
+}
